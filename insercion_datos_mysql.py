72a321a2b81912227ec922c501f45073e4aea2ff
Alexis Araujo <59783026+AlexLinuxGallo91@users.noreply.github.com>
2020-01-21 10:08:24 -0600

    Update insercion_datos_mysql.py

--- insercion_datos_mysql.py
+++ insercion_datos_mysql.py
@@ -23,35 +23,7 @@ mi_cursor.execute("CREATE TABLE IF NOT EXISTS itoc_cat_accounts ("\
 sentencia_insert_sql = 'INSERT INTO itoc_cat_accounts (idaccount, db, server, user, password, service, creation_date, ' \
                        'modif_date, deleted) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)'
 
-mi_cursor.execute(sentencia_insert_sql, (1, 'MTYDG1DS01DB001', 'APCNMBX01', 'mtydg1ds01db001@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (2, 'MTYDG1DS01DB002', 'APCNMBX02', 'mtydg1ds01db002@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (3, 'MTYDG1DS01DB003', 'APCNMBX03', 'mtydg1ds01db003@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (4, 'MTYDG1DS01DB004', 'APCNMBX04', 'mtydg1ds01db004@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (5, 'MTYDG1DS01DB005', 'APCNMBX17', 'mtydg1ds01db005@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (6, 'MTYDG1DS01DB006', 'APCNMBX18', 'mtydg1ds01db006@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (7, 'MTYDG1DS01DB007', 'APCNMBX19', 'mtydg1ds01db007@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (8, 'MTYDG1DS01DB008', 'APCNMBX20', 'mtydg1ds01db008@correo.local', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (9, 'MTYDG1DS01DB009', 'APCNMBX33', 'mtydg1ds01db009@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
-
-mi_cursor.execute(sentencia_insert_sql, (10, 'MTYDG1DS01DB010', 'APCNMBX21', 'mtydg1ds01db010@srv2010.vivetelmex.com', 
-                                        'SUFiav+@92Lp0359@', 'Exchange 2010', '2017-10-26 03:00:40', '2018-02-02 04:47:43', 0))
+# Agregar aqui las sentencias de insercion
 
 mi_cursor.close()
 db_connection.close()
