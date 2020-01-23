d597686df437788267d0715ab958e04f99c020ce
Alexis Araujo <59783026+AlexLinuxGallo91@users.noreply.github.com>
2020-01-23 13:16:52 -0600

    Se elimina Script de prueba
    Se elimina Script de prueba

--- test_json.py
+++
@@ -1,7 +0,0 @@
-import statusJson
-import json
-
-# print(statusJson.JsonPorEnviar.json_a_enviar)
-json.dumps
-y = json.dumps(statusJson.JsonPorEnviar.generar_nuevo_template_json(), sort_keys=False, indent=4)
-print(y)
