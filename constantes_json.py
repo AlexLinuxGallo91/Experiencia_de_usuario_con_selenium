26e3b3c7ce19f4f2c59500bf85138eb41a6c3f87
Alexis Araujo <59783026+AlexLinuxGallo91@users.noreply.github.com>
2020-01-30 16:26:18 -0600

    Se omiten palabras
    Se omiten la palabra "Validacion" en cada titulo de las validaciones

--- constantes_json.py
+++ constantes_json.py
@@ -3,13 +3,13 @@ STATUS_CORRECTO = 'SUCCESS'
 STATUS_FALLIDO = 'FAILED'
 
 # nombres de cada paso
-PASO_1 = "Validaci\u00f3n de Inicio de Sesi\u00f3n en OWA"
-PASO_2 = "Validaci\u00f3n Navegacion entre carpetas"
+PASO_1 = "Inicio de Sesi\u00f3n en OWA"
+PASO_2 = "Navegaci\u00f3n entre carpetas"
 PASO_3 = "Validaci\u00f3n de Cierre de Sesi\u00f3n en OWA"
 
 # nombres de cada sub-paso
-PASO_1_1 = "Inicio de sesion dentro del portal OWA"
-PASO_2_1 = "Navegaci\u00f3n entre carpetas del correo electronico"
+PASO_1_1 = "Inicio de sesi\u00f3n dentro del portal OWA"
+PASO_2_1 = "Navegaci\u00f3n entre carpetas del correo electr\u00f3nico"
 PASO_3_1 = "Cierre de sesi\u00f3n dentro del portal OWA"
 
 # resultados finales/outputs exitosos
@@ -18,7 +18,7 @@ OUTPUT_EXITOSO_2_1 = "Se navega exitosamente entre las carpetas del correo elect
 OUTPUT_EXITOSO_3_1 = "Se cierra exitosamente la sesi\u00f3n dentro del portal OWA"
 
 # resultados finales/outputs fallidos
-OUTPUT_FALLIDO_1_1 = "Se ingresa correctamente la sesi\u00f3n dentro del portal OWA"
+OUTPUT_FALLIDO_1_1_ = "Se ingresa correctamente la sesi\u00f3n dentro del portal OWA"
 OUTPUT_FALLIDO_2_1 = "Se navega exitosamente entre las carpetas del correo electr\u00f3nico"
 OUTPUT_FALLIDO_3_1 = "Se cierra exitosamente la sesi\u00f3n dentro del portal OWA"
 
